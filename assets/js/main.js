24db4a0f6910be49b8264faadd7da91ade85187b
Dharan Gokul R <64479986+DharanGokul@users.noreply.github.com>
2026-01-26 19:55:33 +0530

    pages with hamburger

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,10 @@
 var currentYear = new Date().getFullYear();
 document.getElementById('currentYear').textContent = currentYear;
+
+// Hamburger menu toggle
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('nav');
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('nav-active');
+  hamburger.classList.toggle('toggle');
+});
